ac8d4964ca0a97bdeaba45223af118dc20936090
Enricobusatto <enricobusatto75@gmail.com>
2025-04-03 18:53:57 +0200

    finish

--- esercizio-JS.js
+++ esercizio-JS.js
@@ -4,12 +4,15 @@
  Scrivi un algoritmo per trovare il piÃ¹ grande tra due numeri interi.
 */
 
-let numero1 = 10;
-let numero2 = 20;
+/*
+
+let numero1 = parseInt(prompt("Inserisci un numero PER ESERCIZIO 1"));
+let numero2 = parseInt(prompt("Inserisci un numero PER ESERCIZIO 1"));
+
 if (numero1 > numero2) {
-    console.log("il numero più grande è " + numero1);
+    console.log("ESERCIZIO 1 - il numero più grande è " + numero1);
 } else if (numero1 < numero2) {
-    console.log("il numero più grande è " + numero2);
+    console.log("ESERCIZIO 1 il numero più grande è " + numero2);
 }
 
 /*
@@ -21,9 +24,24 @@ ESERCIZIO 2
   num < 15 - mostra in console "Medium"
   num < 20 - mostra in console "Large"
   num >= 20 - mostra in console "Huge"
+
+let num = prompt("inserisci un numero PER ESERCIZIO 2");
+
+if (num < 5) {
+    console.log("ESERCIZIO 2 - Tiny");
+} else if (num < 10) {
+    console.log("ESERCIZIO 2 - Small");
+} else if (num < 15) {
+    console.log("ESERCIZIO 2 - Medium");
+} else if (num < 20) {
+    console.log("ESERCIZIO 2 - Large");
+} else {
+    console.log("ESERCIZIO 2 - Huge");
+}
+
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+
 
 //ESERCIZI SUI CICLI: 
 
@@ -31,13 +49,30 @@ ESERCIZIO 2
   Mostra i numeri da 0 a 10 (incluso) in ordine ascendente, ma evitando di mostrare i numeri 3 e 8 (suggerimento: ripassa l'uso di "continue").
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+
+
+for (let i = 0; i <= 10 ; i++) {
+    if (i === 3 || i === 8) {
+        continue;
+    }
+    console.log("ESERCIZIO 3 - " + i );
+}
 
 /* ESERCIZIO 11
   Scrivi un ciclo in JavaScript per iterare da 0 a 15. Per ciascun elemento, il ciclo deve controllare the il valore corrente sia pari o dispari, e mostrare il risultato in console.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let pari = ""
+let dispari = ""
+for (let i = 0; i <= 15; i++) {
+    if (i % 2 === 0) {
+        pari += i + " ";
+    } else {
+        dispari += i + " ";
+    }
+} 
+console.log("ESERCIZIO 11 - i numeri pari sono: " + pari);
+console.log("ESERCIZIO 11 - i numeri dispari sono: " + dispari);
 
 //ESERCIZI EXTRA NON OBBLIGATORI
 
@@ -45,7 +80,14 @@ ESERCIZIO 2
   Scrivi un algoritmo per verificare che, dati due numeri interi, il valore di uno di essi sia 8 oppure se la loro addizione/sottrazione sia uguale a 8.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let numero1 = parseInt(prompt("Inserisci un numero PER ESERCIZIO EXTRA 1"));
+let numero2 = parseInt(prompt("Inserisci un numero PER ESERCIZIO EXTRA 1"));
+if (numero1 === 8 || numero2 === 8 || numero1 + numero2 === 8 || numero1 - numero2 === 8) {
+    console.log("ESERCIZIO EXTRA 1 - uno dei numeri è 8 o la loro somma/sottrazione è uguale a 8");
+}
+else {
+    console.log("ESERCIZIO EXTRA 1 - nessuna delle condizioni è vera");
+}
 
 /* ESERCIZIO EXTRA 2
 Stai lavorando su un sito di e-commerce. Stai salvando il saldo totale del carrello dell'utente in una variabile "totalShoppingCart".
